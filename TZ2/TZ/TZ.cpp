8b6984a96ca871ca8abbcdd59414028e4c2ef4e1
vladyanich <vladyanich4@gmail.com>
2024-04-11 16:20:28 +0500

    Исправление по второму ревью

--- TZ2/TZ/TZ.cpp
+++ TZ2/TZ/TZ.cpp
@@ -10,22 +10,36 @@
 #define ID_END 6 
 #define ASCII_NULL 48 
 #define ASCII_NINE 57 
-#define GIVE_END 4 
-#define MONEY_END 11 
+#define MONEY_START 10
+#define END_STRING 2
+#define LIST_DATA_STRING_SIZE 12
+#define START_LOWER_HALF_ASCII 0
+#define END_LOWER_HALF_ASCII 128
+#define CASE_EXIT 4
+#define CHEATCODE_SIZE 17
+#define BUFFER_SIZE 17
+#define MAX_CHEAT_MONEY 1000
+#define MIN_CHEAT_MONEY 1
+
+#define MENU_ADD_ELEMENT_OPTION 1
+#define MENU_SHOW_LIST_OPTION 2
+#define MENU_ENTER_CHEAT_OPTION 3
 
 typedef struct Node  //Объявление структуры 
 {
-	char data[12];
+	char data[LIST_DATA_STRING_SIZE];
 	struct Node* next;
 } Node_t;
 
 Node_t *push_node(Node_t* topNode, char* newData)  //добавление и сдвиг ноды в списке 
 {
 	Node_t *ptr = (Node_t*)malloc(sizeof(Node_t));
-	int i = strlen(newData);
-	memcpy(ptr->data, newData, 12);
-	ptr->next = topNode;
-	return ptr;
+	if (ptr != NULL)
+	{ 
+		memcpy(ptr->data, newData, LIST_DATA_STRING_SIZE);
+		ptr->next = topNode;
+		return ptr;
+	}
 }
 
 void show_nodes(const Node_t* topNode)  //показать список 
@@ -42,89 +56,79 @@ void show_nodes(const Node_t* topNode)  //показать список
 	}
 }
 
-bool check_ascii_node(char* data) // возвращает true или false / Проверка на нижнюю половину табл ASCII 
+bool check_ascii_node(char* data) // Проверка на нижнюю половину табл ASCII 
 {
-	int dataLength = strlen(data), flagLength = 0;
+	int dataLength = strlen(data);
+	int flagLength = 0;
+
 	for (int i = 0; i < dataLength; i++)
 	{
 		data[i] = (int)data[i];
-		if (data[i] < 128 && data[i] >= 0)
+		if (data[i] < END_LOWER_HALF_ASCII && data[i] >= START_LOWER_HALF_ASCII)
 		{
 			flagLength++;
 		}
 	}
-	if (flagLength == dataLength)
-	{
-		return 1; // все символы в нижней половине таблицы ASCII 
-	}
-	else
-	{
-		return 0;
-	}
+	return flagLength == dataLength;
 }
 
 int check_cheat(char* cheatString)
 {
-	char giveMoney[11] = "GIVEMONEY";
-	int money = 4, giveStart = 0, moneyStart = 6, idStart = 4, idNumber = (int)cheatString[idStart], degree = 0, out = 0;
+	int	idStart = 4;
+	int idNumber = (int)cheatString[idStart];
+	int degree = 0;
+	int out = 0;
 
-	while ((giveStart < GIVE_END) && (moneyStart < MONEY_END)) //проверяем givemoney   
+	bool isMaskOk = cheatString[0] == 'G' && cheatString[1] == 'I' && cheatString[2] == 'V' && cheatString[3] == 'E' &&
+		cheatString[6] == 'M' && cheatString[7] == 'O' && cheatString[8] == 'N' && cheatString[9] == 'E' && cheatString[10] == 'Y';  //проверяем GIVEMONEY
+	if (isMaskOk)
 	{
-		if ((giveMoney[giveStart] == cheatString[giveStart]) && (giveMoney[money] == cheatString[moneyStart]))
+		while (idStart < ID_END)    //проверяем id  
 		{
-			giveStart++;
-			moneyStart++;
-			money++;
-		}
-		else
-		{
-			return -1;
-		}
-
-	}
-	while (idStart < ID_END)    //проверяем id  
-	{
-		if ((idNumber >= ASCII_NULL) && (idNumber <= ASCII_NINE))
-		{
-			idStart++;  // проверяем на наличие цифр без символов 
-		}
-		else
-		{
-			return -1;
-		}
-	}
-	for (int count = strlen(cheatString) - 2; count > 10; count--) //проверяем кол-во денег(считываем кол-во денег)   
-	{
-		int b = (int)cheatString[count];
-
-		if ((b >= ASCII_NULL) && (b <= ASCII_NINE)) // проверка на числа  
-		{
-			if ((b - 48) == 0)  //Перевод цифр из ascii в число int  
+			if ((idNumber >= ASCII_NULL) && (idNumber <= ASCII_NINE))
 			{
-				degree++;
+				idStart++;  // проверяем на наличие цифр без символов 
 			}
 			else
 			{
-				out = (pow(10, degree) * (b - 48)) + out;  // Перевод из массива символов в целое число по порядкам 
-				degree++;
+				return -1;
 			}
 		}
-		else
+		for (int count = strlen(cheatString) - END_STRING; count > MONEY_START; count--) //проверяем кол-во денег(считываем кол-во денег)   
 		{
-			return -1;
+			int asciiNumber = (int)cheatString[count];
+			if ((asciiNumber >= ASCII_NULL) && (asciiNumber <= ASCII_NINE)) // проверка на числа  
+			{
+				if ((asciiNumber - ASCII_NULL) == 0)  //Перевод цифр из ascii в число int  
+				{
+					degree++;
+				}
+				else
+				{
+					out = (pow(10, degree) * (asciiNumber - ASCII_NULL)) + out;  // Перевод из массива символов в целое число по порядкам 
+					degree++;
+				}
+			}
+			else
+			{
+				return -1;
+			}
 		}
+		return out;
 	}
-	return out;
+	else
+	{
+		return -1;
+	}
 }
 
 void repair_length(char *data)
 {
-	int i = strlen(data+1);
-	for (int n = 0; n <= i; n++)
+	for (int stringSize = 0; stringSize <= LIST_DATA_STRING_SIZE; stringSize++)
 	{
-		if (data[i] == '\n')
+		if (data[stringSize] == '\n')
 		{
-			data[i] = '\0';
+			data[stringSize] = '\0';
 		}
 	}
 }
@@ -132,59 +136,55 @@ void repair_length(char *data)
 int main()
 {
 	int number = 0;
-	char buf[10];
-
+	char buffer[BUFFER_SIZE];
 	Node_t* topNode = NULL;
 
-	while (number != 4)
+	while (number != CASE_EXIT)
 	{
-		char* dataNode = (char*)malloc(12 * sizeof(char)); //статически выделить
+		char dataNode[LIST_DATA_STRING_SIZE];
 		printf("\n1. Add node");
 		printf("\n2. Show list");
 		printf("\n3. Enter cheat");
 		printf("\n4. Quit\n");
 
-		fgets(buf, sizeof(buf), stdin);
-		number = atoi(buf);
+		fgets(buffer, sizeof(buffer), stdin);
+		number = atoi(buffer);
 
 		switch (number)
 		{
-		case 1:    // Добавить элемент 
-		{
-			fgets(dataNode, 12, stdin);
-			if (check_ascii_node(dataNode))
+			case MENU_ADD_ELEMENT_OPTION:    // Добавить элемент 
 			{
-				repair_length(dataNode);
-				topNode = push_node(topNode, dataNode);
+				fgets(dataNode, LIST_DATA_STRING_SIZE, stdin);
+				if (check_ascii_node(dataNode))
+				{
+					repair_length(dataNode);
+					topNode = push_node(topNode, dataNode);
+				}
+				else
+				{
+					printf("Used upper half of ASCII table!\n");
+				}
 			}
-			else
+			break;
+			case MENU_SHOW_LIST_OPTION:  // Отобразить список 
 			{
-				printf("Used upper half of ASCII table!\n");
+				show_nodes(topNode);
 			}
-		}
-		break;
-		case 2:  // Отобразить список 
-		{
-			show_nodes(topNode);
-		}
-		break;
-		case 3:  //Ввод чит-кода 
-		{
-			char* cheat = (char*)malloc(17 * sizeof(char));
-			fgets(cheat, 17, stdin);
-			short valueCheat = check_cheat(cheat);
-			if (valueCheat >= 1 && valueCheat <= 1000)
+			break;
+			case MENU_ENTER_CHEAT_OPTION:  //Ввод чит-кода 
 			{
-				printf("Money add: %d", valueCheat);
+					fgets(buffer, CHEATCODE_SIZE, stdin);
+					short valueCheat = check_cheat(buffer);
+					if (valueCheat >= MIN_CHEAT_MONEY && valueCheat <= MAX_CHEAT_MONEY)
+				{
+					printf("\nMoney add: %d\n", valueCheat);
+				}
+				else
+				{
+					printf("\nMoney add: 0\n");
+				}
 			}
-			else
-			{
-				printf("Money add: 0");
-			}
-			free(cheat);
-		}
-		break;
-		free(dataNode);
+			break;
 		}
 	}
 	return 0;
